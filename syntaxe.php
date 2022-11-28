235157afe40a271a4764785a12de186ed0cfe3c1
patrick ouambo <patgp04@gmail.com>
2022-11-28 17:33:00 +0100

    Update syntaxe.php

--- syntaxe.php
+++ syntaxe.php
@@ -1,1 +1,14 @@
-Syntaxe PHP
+<?php
+  /*
+  * index.html
+  * >> <h1>Bonjour tout le monde</h1>
+  * 1. PAGE STATIQUE
+  * 2. 'index', identifie par le serveur
+  *
+  * index.php
+  * 2. PAGE DYNAMIQUE
+  * >> <h1>
+  * >>   <?php echo "Bonjour tout le monde"; ?>
+  * >> </h1>
+  * >>
+  */
